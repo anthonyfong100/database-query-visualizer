2a8c23d00a95942c16d7dc258eca62454049bc15
Nigel Lee <lee.nigel@hotmail.com>
2020-11-25 22:26:35 +0800

    run analyse queries

--- client.py
+++ client.py
@@ -25,4 +25,4 @@ def explain():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- sql_parser/main.py
+++ sql_parser/main.py
@@ -24,9 +24,14 @@ def parse(query):
     # Find table names that the columns can be found in
     for column in columns:
         findTableQuery = "select t.table_schema, t.table_name from information_schema.tables t inner join information_schema.columns c on c.table_name = t.table_name and c.table_schema = t.table_schema where c.column_name = '" + column + "' and t.table_schema not in ('information_schema', 'pg_catalog') and t.table_type = 'BASE TABLE' order by t.table_schema;"
-        execute = db.prepare(findTableQuery)
+        findTable = db.prepare(findTableQuery)
         # TODO: run analyze on each column
-        print(execute())
+        table = findTable.first()[1]
+
+        runAnalyseMaxQuery = "SELECT MAX(" + column + ")" + " FROM " + table
+        runAnalyseMax = db.prepare(runAnalyseMaxQuery)
+        runAnalyseMinQuery = "SELECT MIN(" + column + ")" + " FROM " + table
+        runAnalyseMin = db.prepare(runAnalyseMinQuery)
     
     # TODO: using min and max values of each column, split into 10 buckets and generate 10 different queries
         
