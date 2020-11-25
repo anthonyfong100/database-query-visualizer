14fd6080f280fec757abd2a3745e04617f0e4c9d
Anthony <anthonyfong100@gmail.com>
2020-11-25 23:49:32 +0800

    adding dependency psycopg

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 import time
 
-from flask import Flask, redirect, render_template, request, url_for
 import psycopg2
+from flask import Flask, redirect, render_template, request, url_for
+
 from sql_parser.main import parse
 
 app = Flask(__name__)
@@ -18,10 +19,12 @@ def explain():
         return redirect("/")
     query = request.form["queryText"]
     parse(query)
-    explanation = ['Anthony is a google intern', 'Anthony is a fb intern', 'Anthony is an apple intern']
-    return render_template('index.html', query=query, explanation=explanation)
-
-
+    explanation = [
+        "Anthony is a google intern",
+        "Anthony is a fb intern",
+        "Anthony is an apple intern",
+    ]
+    return render_template("index.html", query=query, explanation=explanation)
 
 
 @app.route("/test_explain", methods=["GET"])
@@ -31,52 +34,58 @@ def test_explain():
     dbname = "picasso"
     user = "postgres"
     password = "postgres"
-    conn_string = "host='%s' port='%s' dbname='%s' user='%s' password='%s'"%(
-            host, port, dbname, user, password)
+    conn_string = "host='%s' port='%s' dbname='%s' user='%s' password='%s'" % (
+        host,
+        port,
+        dbname,
+        user,
+        password,
+    )
     conn = psycopg2.connect(conn_string)
     cursor = conn.cursor()
     query = """select
-	s_name,
-	count(*) as numwait
+    s_name,
+    count(*) as numwait
 from
-	supplier,
-	lineitem l1,
-	orders,
-	nation
+    supplier,
+    lineitem l1,
+    orders,
+    nation
 where
-	s_suppkey = l1.l_suppkey
-	and o_orderkey = l1.l_orderkey
-	and o_orderstatus = 'F'
-	and exists (
-		select
-			*
-		from
-			lineitem l2
-		where
-			l2.l_orderkey = l1.l_orderkey
-			and l2.l_suppkey <> l1.l_suppkey
-	)
-	and not exists (
-		select
-			*
-		from
-			lineitem l3
-		where
-			l3.l_orderkey = l1.l_orderkey
-			and l3.l_suppkey <> l1.l_suppkey
-			and l3.l_receiptdate > l3.l_commitdate
-	)
-	and s_nationkey = n_nationkey
-	and n_name = 'SAUDI ARABIA'
+    s_suppkey = l1.l_suppkey
+    and o_orderkey = l1.l_orderkey
+    and o_orderstatus = 'F'
+    and exists (
+        select
+            *
+        from
+            lineitem l2
+        where
+            l2.l_orderkey = l1.l_orderkey
+            and l2.l_suppkey <> l1.l_suppkey
+    )
+    and not exists (
+        select
+            *
+        from
+            lineitem l3
+        where
+            l3.l_orderkey = l1.l_orderkey
+            and l3.l_suppkey <> l1.l_suppkey
+            and l3.l_receiptdate > l3.l_commitdate
+    )
+    and s_nationkey = n_nationkey
+    and n_name = 'SAUDI ARABIA'
 group by
-	s_name
+    s_name
 order by
-	numwait desc,
-	s_name"""
+    numwait desc,
+    s_name"""
     cursor.execute("EXPLAIN (FORMAT JSON) " + query)
     plan = cursor.fetchall()
     query_plan = plan[0][0][0]["Plan"]
     return query_plan
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
