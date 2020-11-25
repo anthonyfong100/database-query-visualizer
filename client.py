062304b4bb12e4092cd5deef7be23b39e71f2efc
Nigel Lee <lee.nigel@hotmail.com>
2020-11-25 17:55:46 +0800

    added parser module
    tweaked frontend

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import time
+from parser.main import parse
 
 app = Flask(__name__)
 
@@ -12,6 +13,7 @@ def explain():
     if(request.method == 'GET'):
         return redirect('/')
     query = request.form['queryText']
+    parse(query)
     time.sleep(2) # TODO: We can call the query parser here
     explanation = ['Anthony is a google intern', 'Anthony is a fb intern', 'Anthony is an apple intern']
     return render_template('index.html', query=query, explanation=explanation)
---
+++ parser/main.py
@@ -0,0 +1,2 @@
+def parse(query):
+    print(query)
