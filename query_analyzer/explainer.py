9259e509f6cb194adbd036bad7df4bbc07683bdd
chinzhiweiblank <chinzhiweiblank@gmail.com>
2020-11-28 22:37:45 +0800

    Change relative imports to absolute imports

--- query_analyzer/explainer.py
+++ query_analyzer/explainer.py
@@ -1,29 +1,29 @@
-from .explainers.aggregate_explain import aggregate_explain
-from .explainers.append_explain import append_explain
-from .explainers.cte_explain import cte_explain
-from .explainers.function_scan_explain import (
+from query_analyzers.explainers.aggregate_explain import aggregate_explain
+from query_analyzers.explainers.append_explain import append_explain
+from query_analyzers.explainers.cte_explain import cte_explain
+from query_analyzers.explainers.function_scan_explain import (
     function_scan_explain,
 )
-from .explainers.group_explain import group_explain
-from .explainers.hash_explain import hash_explain
-from .explainers.hash_join_explain import hash_join_explain
-from .explainers.index_scan_explain import (
+from query_analyzers.explainers.group_explain import group_explain
+from query_analyzers.explainers.hash_explain import hash_explain
+from query_analyzers.explainers.hash_join_explain import hash_join_explain
+from query_analyzers.explainers.index_scan_explain import (
     index_only_scan_explain,
     index_scan_explain,
 )
-from .explainers.limit_explain import limit_explain
-from .explainers.materializer_explain import materialize_explain
-from .explainers.merge_join_explain import merge_join_explain
-from .explainers.nested_loop_explain import nested_loop_explain
-from .explainers.seq_scan_explain import seq_scan_explain
-from .explainers.setop_explain import setop_explain
-from .explainers.sort_explain import sort_explain
-from .explainers.subquery_scan_explain import (
+from query_analyzers.explainers.limit_explain import limit_explain
+from query_analyzers.explainers.materializer_explain import materialize_explain
+from query_analyzers.explainers.merge_join_explain import merge_join_explain
+from query_analyzers.explainers.nested_loop_explain import nested_loop_explain
+from query_analyzers.explainers.seq_scan_explain import seq_scan_explain
+from query_analyzers.explainers.setop_explain import setop_explain
+from query_analyzers.explainers.sort_explain import sort_explain
+from query_analyzers.explainers.subquery_scan_explain import (
     subquery_scan_explain,
 )
-from .explainers.test_explain import test_explain
-from .explainers.unique_explain import unique_explain
-from .explainers.values_scan_explain import values_scan_explain
+from query_analyzers.explainers.test_explain import test_explain
+from query_analyzers.explainers.unique_explain import unique_explain
+from query_analyzers.explainers.values_scan_explain import values_scan_explain
 
 
 class Explainer(object):
