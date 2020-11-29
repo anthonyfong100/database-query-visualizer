cae2f9f1962f406cdbe931e05b2bfe60e1ee9edf
chinzhiweiblank <chinzhiweiblank@gmail.com>
2020-11-29 17:00:00 +0800

    Test Pre-commit, add bold strings

--- query_analyzer/explainers/merge_join_explain.py
+++ query_analyzer/explainers/merge_join_explain.py
@@ -1,11 +1,16 @@
+from color import bold_string
+
+
 def merge_join_explain(query_plan):
     result = "The results from sub-operations are joined using Merge Join"
 
-    if 'Merge Cond' in query_plan:
-        result += ' with condition ' + query_plan['Merge Cond'].replace("::text", "")
+    if "Merge Cond" in query_plan:
+        result += " with condition " + bold_string(
+            query_plan["Merge Cond"].replace("::text", "")
+        )
 
-    if 'Join Type' == 'Semi':
-        result += ' but only the row from the left relation is returned'
-    
-    result += '.'
+    if "Join Type" == "Semi":
+        result += " but only the row from the left relation is returned"
+
+    result += "."
     return result
